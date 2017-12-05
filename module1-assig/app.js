43a03575e47cb68209157455bc2a04bdf2c14849
Enrico Esposito <e.esposito_ext@itsoftware.it>
2017-12-05 15:45:30 +0100

    First exercise

--- module1-assig/app.js
+++ module1-assig/app.js
@@ -2,14 +2,35 @@
   'use strict';
 
   angular.module('LunchCheck', [])
-  .controller('checkCtrl', checkCtrl);
+  .controller('LunchCheckController', LunchCheckController);
 
-  checkCtrl.$injection = ['$scope'];
-  function checkCtrl($scope) {
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+// Comma separed items
+// if <= 3 "Enjoy!" under text box
+// else "Too much!" under text box
+// if empty "Please enter data first" under text box
 
-    $scope.value;
-    $scope.msgOK;
-    $scope.msgKO;
+//    $scope.value;
+    var msgOK = "Enjoy!";
+    var msgKO = "Too much!";
+    //
+    $scope.doCheck = function () {
+
+      if(!$scope.value) {
+
+        $scope.msg = 'Please enter data first';
+        return;
+      }
+
+      if ($scope.value.split(',').length<=3) {
+        $scope.msg = msgOK;
+      } else {
+        $scope.msg = msgKO;
+      }
+
+    };
+
   }
 
 })()
